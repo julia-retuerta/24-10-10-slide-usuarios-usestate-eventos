19727d29e5e2893361818b6c9d992fb8c94f03e5
julia-retuerta <164010086+julia-retuerta@users.noreply.github.com>
2024-10-21 15:17:36 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>REACT</h1>;
+    </>
+  );
+};
+
+export default App;
